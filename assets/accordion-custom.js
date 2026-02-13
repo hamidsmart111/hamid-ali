230a0e3321872f4a4170f8f89bd9b654b77b0c88
Hamid Ali <hamid@mobiiworld.com>
2026-02-13 20:53:26 +0530

    button test.

--- assets/accordion-custom.js
+++ assets/accordion-custom.js
@@ -105,3 +105,19 @@ class AccordionCustom extends HTMLElement {
 if (!customElements.get('accordion-custom')) {
   customElements.define('accordion-custom', AccordionCustom);
 }
+
+document.addEventListener("click", function(e) {
+
+  if (e.target.classList.contains("product-card__modal-trigger")) {
+    const id = e.target.dataset.productId;
+    document.getElementById("quick-view-" + id).classList.add("active");
+  }
+
+  if (
+    e.target.classList.contains("quick-view-modal__overlay") ||
+    e.target.classList.contains("quick-view-modal__close")
+  ) {
+    e.target.closest(".quick-view-modal").classList.remove("active");
+  }
+
+});
